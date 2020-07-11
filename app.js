a9c46714eba15dcce979efc97aeb3b3257ce39da
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-07-11 16:39:21 +0900

    [Update]jQueryLesson

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 $(function(){
-	$('button').on('click', function(){
-		$('ul').children().css('color', 'red');
+	$('.box1').slideDown(function(){
+		$('.box1').css({
+			'background-color': '#0000FF',
+			'width': '200px',
+			'height': '100px'
+		}).slideUp();
 	});
 });
